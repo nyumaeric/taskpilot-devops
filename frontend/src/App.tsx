1952bf6d51076ff877121a8b550e4f9f79d1859f
nyumaeric <e.nyumajr@alustudent.com>
2025-07-02 00:23:41 +0200

    TaskPliot  project in progress

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import TaskManager from "./components/TaskManager";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <TaskManager />
+    </div>
+  );
+}
+
+export default App;
